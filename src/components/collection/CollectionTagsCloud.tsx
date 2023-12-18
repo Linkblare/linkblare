1e92f65797bd3e8e8bac0851bfd060bd2570b91c
shivam-687 <shivamsi687@gmail.com>
2023-12-18 23:19:58 +0530

    update

--- src/components/collection/CollectionTagsCloud.tsx
+++ src/components/collection/CollectionTagsCloud.tsx
@@ -7,46 +7,48 @@ import QueryTags from '../query-tag/QueryTags';
 import InifiniteItemList from '../grid/InfiniteItemList';
 import { nanoid } from 'nanoid';
 import { Skeleton } from '../ui/skeleton';
+import { TagOut } from '@/schema/tag-schema';
+import { Button } from '../ui/button';
+import { cn } from '@/lib/utils';
 
 const CollectionTagsCloud = ({
   collectionId
 }: {
   collectionId: number;
 }) => {
-  const { data, isLoading, fetchNextPage, hasNextPage, refetch } = api.tags.infintList.useInfiniteQuery({ targetCollection: collectionId }, {
+  const { data, isLoading, fetchNextPage, hasNextPage, refetch } = api.tags.infintList.useInfiniteQuery({ targetCollection: collectionId, sort: { name: 'asc' }, take: 10 }, {
     getNextPageParam: (lastPage) => lastPage.nextCursor
   });
-  const items = data?.pages.reduce((acc, page) => acc.concat(page.items), [] as any[])
+  const items = data?.pages.reduce((acc, page) => acc.concat(page.items), [] as TagOut[])
   return (
-    <div className='rounded-2xl border max-w-3xl mx-auto mb-10'>
-      <InifiniteItemList
+    <div className={cn([
+      'rounded-2xl border max-w-3xl mx-auto mb-10 relative bg-card ',
+      {'pb-14': hasNextPage}
+    ])}>
 
-                loading={isLoading}
-                loader={<Skeleton className='h-10 w-full' />}
-                dataLength={items?.length ?? 0}
-                fetchNextPage={fetchNextPage}
-                hasMore={hasNextPage}
-                refetch={refetch}
-            >
-                {
-                    data?.pages.map(page =>
-                      <QueryTags
-                      key={nanoid()}
-                      className='justify-center'
-                      queryKey='tag'
-                      boxMode="container"
-                      queryInputs={page.items.map(it => ({
-                        value: it.name,
-                        lable: it.name,
-                        toggleMode: true,
-                        singleMode: false,
-              
-                      })) ?? []}
-                      loading={isLoading}
-                    />
-                    )
-                }
-            </InifiniteItemList>
+      
+        <QueryTags
+          key={nanoid()}
+          className='justify-center max-h-48 overflow-y-auto scrollbar-thin scrollbar-thumb-primary'
+          queryKey='tag'
+          boxMode="container"
+          queryInputs={items?.map(it => ({
+            value: it.name,
+            lable: it.name,
+            toggleMode: true,
+            singleMode: false,
+
+          })) ?? []}
+          loading={isLoading}
+        />
+      
+
+      {
+        hasNextPage && <div className='absolute bottom-0 left-0 w-full flex items-center justify-center'>
+        <Button disabled={isLoading} onClick={() => void fetchNextPage()}>Load More</Button>
+      </div>
+      }
+
     </div>
   )
 }
